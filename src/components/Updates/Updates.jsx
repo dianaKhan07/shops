bcf440d4ee7f35515650ef0bad65410ab3bae922
dianaKhan07 <126963862+dianaKhan07@users.noreply.github.com>
2023-07-21 01:29:01 +0600

    Add files via upload

---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Updates.css'
+import { UpdatesData } from '../../data/Data'
+const Updates = () => {
+  return (
+    <div className='Updates'>
+      {UpdatesData.map((update)=>{
+        return(
+            <div className="update">
+                <img src={update.img} alt="" />
+                <div className="not1">
+                    <div style={{marginBottom: '0.5rem'}}> 
+                        <span>{update.name}</span>
+                        <span>{update.not1}</span>
+                    </div>
+                    <span>{update.time}</span>
+                </div>
+            </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Updates
